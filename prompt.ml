47717ad42ebbab45ada34582202684fea80aba2b
Michael Bacarella <mbacarella@nyc-qws-009.delacy.com>
2008-11-03 14:04:31 -0500

    testing git-push

--- prompt.ml
+++ prompt.ml
@@ -6,6 +6,7 @@ open Unix
 let read_password () =
   Printf.printf "Safe combination: ";
   read_line()
+
 (*
   let term_init = tcgetattr stdin in
   let term_no_echo = {term_init with c_echo = false; } in
