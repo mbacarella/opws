d053bfe2147395734710898badfdd742c3c12489
Michael Bacarella <mbacarella@nyc-qws-009.delacy.com>
2008-11-07 16:12:26 -0500

    Added GPL to twofish.ml

--- twofish.ml
+++ twofish.ml
@@ -1,8 +1,23 @@
-(*
-  An implementation of Twofish, by Michael Bacarella.
+(* An implementation of Twofish in OCaml
+   Copyright (C) 2008  Michael Bacarella.
 
-  The algorithm below has been implemented using Guido Flohr's CPAN Crypt::Twofish_PP
-  as a reference.  Any flaws in this program are my own shame, however.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+   The algorithm below has been implemented using Guido Flohr's
+   CPAN Crypt::Twofish_PP as a reference.  Any flaws in this program
+   are, however, my own fault.
 *)
 
 let and32 x n = Int32.logand x n
